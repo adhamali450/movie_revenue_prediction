77688fab8220d10598d7fbc5a470f74bc25ab504
bavlly <pavllymagid04@gmail.com>
2022-04-19 03:33:48 +0200

    used animated and directors as features and completed animation

--- predict.py
+++ predict.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -8,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import Ridge
-from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
 
 
@@ -20,53 +18,44 @@ def Feature_Encoder(X,cols):
            
     return X
 
-def featureScaling(X,a,b):
-    X = np.array(X)
-    Normalized_X=np.zeros((X.shape[0],X.shape[1]))
-    for i in range(X.shape[1]):
-        Normalized_X[:,i]=((X[:,i]-min(X[:,i]))/(max(X[:,i])-min(X[:,i])))*(b-a)+a
-    return Normalized_X
 
-
-
-
-data = pd.read_csv('datasets/[MERGED-COMPLETE]movies_revenue.csv')
+data = pd.read_csv('datasets/[ANIMATED]movie-revenues.csv')
 
 #data cleaning
 data.dropna(how='any',inplace=True)
 data['revenue'] = data['revenue'].str[1:].str.replace(',','').astype("float32").astype("int32")
-print(data.shape)
 data['release_date'] = pd.to_datetime(data['release_date'])
 data['release_date'] = data['release_date'].view('int64')
 
-cols=('movie_title','genre','MPAA_rating','directors')
+cols=('movie_title','genre','MPAA_rating','directors', 'animated')
 data = Feature_Encoder(data,cols)
 
 X = data.iloc[:,0:] #Features
 Y=data['revenue'] #Label
-
-# X = featureScaling(X,0,10)
-# scaler = StandardScaler()
-# X = scaler.fit_transform(X)
 
 # Feature Selection
 # Get the correlation between the features
 movie_data = data.iloc[:,:]
 corr = movie_data.corr()
 
-#Correlation training features with the Value
-top_feature = corr.index[abs(corr['revenue'])>=0.09]
+
+top_feature = corr.index[abs(corr['revenue']) > 0.009]
 # Correlation plot
-plt.subplots(figsize=(12, 8))
-top_corr = movie_data[top_feature].corr()
-sns.heatmap(top_corr, annot=True)
-plt.show()
-top_feature = top_feature.delete(-1)
+# plt.subplots(figsize=(12, 8))
+# top_corr = movie_data[top_feature].corr()
+# sns.heatmap(top_corr, annot=True)
+# plt.show()
+# top_feature = top_feature.delete(-1)
 X = X[top_feature]
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.20,shuffle=True,random_state=120)
+#feature scaling
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 
-poly_features = PolynomialFeatures(degree=3)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.40,shuffle=True,random_state=42)
+
+poly_features = PolynomialFeatures(degree=2)
 
 # transforms the existing features to higher degree features.
 X_train_poly = poly_features.fit_transform(X_train)
@@ -84,14 +73,13 @@ print('Mean Square Error Polynomial train => ', metrics.mean_squared_error(y_tra
 print("############################################################################################")
 
 
-rdg = Ridge(alpha = 0.5 , normalize = True)
+rdg = Ridge(alpha = 0.1 , normalize = True)
 rdg.fit(X, Y)
 prediction3 = rdg.predict(X_test)
-print('Mean Square Error ridge => ', metrics.mean_squared_error(y_test, prediction3))
+print('Mean Square Error ridge test => ', metrics.mean_squared_error(y_test, prediction3))
+
+prediction3 = rdg.predict(X_train)
+print('Mean Square Error ridge train =>', metrics.mean_squared_error(y_train, prediction3))
 
 
-regr = MLPRegressor(random_state=1, max_iter=500).fit(X_train, y_train)
-
-pred = regr.predict(X_test)
-print('Mean Square Error MLP => ', metrics.mean_squared_error(y_test, pred))
 
